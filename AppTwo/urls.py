1bfaef8af556107df4d7821ffbf2d114f7c75127
inoyka <simonmarkholland@gmail.com>
2018-02-07 13:46:31 +0700

    Added help view

--- AppTwo/urls.py
+++ AppTwo/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from AppTwo import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('help/', views.help, name='help'),
-    path('admin/', admin.site.urls),
-
+    path('', views.help, name='help'),
+    path('help', views.help, name='help'),
 ]
--- AppTwo/views.py
+++ AppTwo/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>Hello World</em>")
+    my_dict = {'my_text':'My text inserted from views.py 2'}
+    return render(request, 'AppTwo/index.html', context= my_dict)
+
+
+def help(request):
+    help_dict = {'my_help':'My help!!! text inserted from views.py'}
+    return render(request, 'AppTwo/help.html', context= help_dict)
